23a96ee6ce85a6930143d766ca964195e583ac45
matsuokanao <matsuokanao1029@gmail.com>
2020-06-25 18:02:56 +0900

    first

---
+++ ApplicationSystem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ApplicationSystem
+//
+//  Created by 松岡奈央 on 2020/06/25.
+//  Copyright © 2020 松岡奈央. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ View/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  ApplicationSystem
+//
+//  Created by 松岡奈央 on 2020/06/25.
+//  Copyright © 2020 松岡奈央. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
