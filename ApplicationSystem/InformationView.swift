424044a4a01d734a7b871b54546f6cad895df42f
matsuokanao <matsuokanao1029@gmail.com>
2020-07-09 19:19:50 +0900

    エントリー画面実装

--- ApplicationSystem/InformationView.swift
+++ ApplicationSystem/InformationView.swift
@@ -10,15 +10,17 @@ import SwiftUI
 import FirebaseFirestore
 
 struct InformationView: View {
+    @State var color = Color.black.opacity(0.7)
     @ObservedObject var data = getCompleteData()
-
+    @State var show = false
     var body: some View {
         VStack{
             if self.data.datas.count != 0{
-                ScrollView(.vertical, showsIndicators: false){
-                VStack{
-                    ForEach(self.data.datas,id: \.id){i in
-                       Securitys(informationdata: i)
+                    ScrollView(.vertical, showsIndicators: false){
+                    VStack{
+                        ForEach(self.data.datas,id: \.id){i in
+                            CellInformationView(informationdata: i)
+                        
                         }
                     }
                 }
@@ -33,55 +35,25 @@ struct InformationView_Previews: PreviewProvider {
     }
 }
 
-struct Securitys: View{
-    @State var color = Color.black.opacity(0.7)
-    @State var show = false
-    var informationdata : complete
-    @State var idemail = ""
-    var body: some View {
-      
-        VStack{
-                  Text("所属組織名")
-                  TextField("所属組織を入力して下さい", text: $idemail)
-                      .padding()
-                      .background(RoundedRectangle(cornerRadius: 4).stroke(self.idemail != "" ? Color("PinkRed") : self.color,lineWidth:  2))
-            Button(action: {
-                                        
-                self.show.toggle()
-                    }) {
-                                    
-            Image(systemName: "arrow.right")
-                .font(.body)
-                .foregroundColor(.black)
-                .padding(14)
-                                
-            }.background(Color.yellow)
-                .clipShape(Circle())
-                                
-            .padding(.horizontal)
-                .padding(.bottom,6)
-    
-    .background(Color.white)
-    .sheet(isPresented: self.$show){
-        if self.informationdata.idEmail == self.idemail{
-    CellInformationView(informationdata: self.informationdata)
-              Spacer()
-            }
-            }
-        }
-    }
-}
+
         
     
 
 
 struct CellInformationView :View {
-    var informationdata : complete
+    @State var color = Color.black.opacity(0.7)
     @State var show = false
-    var idmail = ""
+    var informationdata : complete
+    @State var idmail = ""
     var body: some View {
         
-        HStack{
+        VStack{
+            Text("試合登録時に入力したメールアドレスを入力して下さい")
+            TextField("メールアドレスを入力して下さい", text: $idmail)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 4).stroke(self.idmail != "" ? Color("PinkRed") : self.color,lineWidth:  2))
+
+            if informationdata.idEmail == self.idmail {
             VStack(alignment: .leading){
             Text(informationdata.name).font(.title).fontWeight(.heavy)
             Text(informationdata.representativeName).fontWeight(.heavy).font(.body)
@@ -91,10 +63,11 @@ struct CellInformationView :View {
             self.show.toggle()
                 }) {
                                         
-        Image(systemName: "arrow.right")
+        Text("確認する")
                 .font(.body)
                 .foregroundColor(.black)
                 .padding(14)
+                }
             }
         }
     }
