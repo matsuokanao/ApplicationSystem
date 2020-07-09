0a951e0d57ac45adc67e5b1fe00ee86f2ebff87f
matsuokanao <matsuokanao1029@gmail.com>
2020-07-09 21:50:34 +0900

    エントリー確認画面実装

--- ApplicationSystem/InformationView.swift
+++ ApplicationSystem/InformationView.swift
@@ -5,24 +5,27 @@
 //  Created by 松岡奈央 on 2020/07/08.
 //  Copyright © 2020 松岡奈央. All rights reserved.
 //
-
 import SwiftUI
 import FirebaseFirestore
+
 
 struct InformationView: View {
     @State var color = Color.black.opacity(0.7)
     @ObservedObject var data = getCompleteData()
     @State var show = false
+    @State var idmail = ""
     var body: some View {
         VStack{
-            if self.data.datas.count != 0{
-                    ScrollView(.vertical, showsIndicators: false){
+            Text("試合登録時に入力したメールアドレスを入力して下さい")
+            TextField("メールアドレスを入力して下さい", text: $idmail)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 4).stroke(self.idmail != "" ? Color("PinkRed") : self.color,lineWidth:  2))
+
                     VStack{
-                        ForEach(self.data.datas,id: \.id){i in
-                            CellInformationView(informationdata: i)
+                        List(self.data.datas,id: \.id){i in
+                            CellInformationView(informationdata: i, data: self.idmail)
                         
-                        }
-                    }
+                        
                 }
             }
         }
@@ -44,16 +47,13 @@ struct CellInformationView :View {
     @State var color = Color.black.opacity(0.7)
     @State var show = false
     var informationdata : complete
-    @State var idmail = ""
+    var data :String
+    
     var body: some View {
         
         VStack{
-            Text("試合登録時に入力したメールアドレスを入力して下さい")
-            TextField("メールアドレスを入力して下さい", text: $idmail)
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 4).stroke(self.idmail != "" ? Color("PinkRed") : self.color,lineWidth:  2))
 
-            if informationdata.idEmail == self.idmail {
+            if informationdata.idEmail == data {
             VStack(alignment: .leading){
             Text(informationdata.name).font(.title).fontWeight(.heavy)
             Text(informationdata.representativeName).fontWeight(.heavy).font(.body)
